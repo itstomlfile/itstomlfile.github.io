96fdf09b12f3a65bbc270c295a307510ddadb261
Tom C <tom.charlesworth1997@gmail.com>
2023-09-29 16:49:17 +0100

    Add simple contact form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm, ValidationError } from '@formspree/react';
+import { SubmissionError } from '@formspree/core';
+import { useState, ChangeEvent } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function ContactForm() {
+  const FORMSPREE_ID: string | undefined = import.meta.env.VITE_FORMSPREE_ID || "mbjvpdkr";
+  const [name, setName] = useState<string>('');
+  const [state, handleSubmit] = useForm(FORMSPREE_ID as string);
+
+  if (state.succeeded) {
+    return (
+      <div className="text-center mt-10 bg-gray-200 p-8 rounded-md shadow-md">
+        <p className="text-gray-700 font-semibold">Thank you for your message, {name}. I will be in touch ASAP.</p>
+      </div>
+    );
+  }
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>My Portfolio Site</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <form onSubmit={handleSubmit} className="bg-gray-200 p-8 rounded-md shadow-md w-full max-w-lg mx-auto">
+     <label htmlFor="fullname" className="block text-gray-600 font-medium mb-2">
+        Full Name
+      </label>
+      <input
+        id="fullname"
+        type="text"
+        name="fullname"
+        value={name}
+        onChange={handleNameChange}
+        className="w-full p-2 border border-gray-400 rounded-md mb-3 focus:outline-none focus:ring-2 focus:ring-gray-500"
+      />
+      <ValidationError 
+        prefix="Full Name" 
+        field="fullname"
+        errors={state.errors}
+        className="text-red-600"
+      />
+
+      <label htmlFor="email" className="block text-gray-600 font-medium mb-2">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+        className="w-full p-2 border border-gray-400 rounded-md mb-3 focus:outline-none focus:ring-2 focus:ring-gray-500"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+        className="text-red-600"
+      />
+
+      <label htmlFor="message" className="block text-gray-600 font-medium mb-2">
+        Message
+      </label>
+      <textarea
+        id="message"
+        name="message"
+        className="w-full p-2 border border-gray-400 rounded-md mb-3 focus:outline-none focus:ring-2 focus:ring-gray-500"
+        rows={4}
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors as SubmissionError<any> | null}
+        className="text-red-600"
+      />
+
+      <button type="submit" disabled={state.submitting} className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-700">
+        Submit
+      </button>
+    </form>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
