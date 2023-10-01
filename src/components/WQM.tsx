142b0a25a7375fef4146b8a57bb264e24cbeb06d
Tom C <tom.charlesworth1997@gmail.com>
2023-10-01 22:29:38 +0100

    Add static link for WQM

--- src/components/WQM.tsx
+++ src/components/WQM.tsx
@@ -1,12 +1,10 @@
-import { Link } from 'react-router-dom';
-
 function WorkQueueButton() {
   return (
-    <Link to="/work-queue-management-ui/">
+    <a href="https://tomcharlesworth1997.github.io/work-queue-management-ui/" target="_blank" rel="noopener noreferrer">
       <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700">
         Work Queue Management
       </button>
-    </Link>
+    </a>
   );
 }
 
