f33d27db9bfefa8b9db90df3ec29d9926bece560
Tom C <tom.charlesworth1997@gmail.com>
2023-10-01 22:03:24 +0100

    Fix link path

--- src/components/WQM.tsx
+++ src/components/WQM.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function WorkQueueButton() {
   return (
-    <Link to="/work-queue-management-ui">
+    <Link to="/work-queue-management-ui/">
       <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700">
         Work Queue Management
       </button>
