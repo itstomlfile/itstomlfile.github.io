39384b98f9b533cde1dcf3ea2f330b7f988c95cb
Tom C <tom.charlesworth1997@gmail.com>
2023-10-02 19:12:00 +0100

    Internal link for WQM button

--- src/components/WQM.tsx
+++ src/components/WQM.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function WorkQueueButton() {
   return (
-    <a href="https://tomcharlesworth1997.github.io/work-queue-management-ui/" target="_blank" rel="noopener noreferrer">
+    <Link to="/work-queue-management">
       <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700">
         Work Queue Management
       </button>
-    </a>
+    </Link>
   );
 }
 
