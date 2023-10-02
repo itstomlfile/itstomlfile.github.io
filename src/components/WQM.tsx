4a131b39496cdb0a80778606c162c3049ace08ac
Tom C <tom.charlesworth1997@gmail.com>
2023-10-02 20:40:26 +0100

    Add WQM service and UI to demonstrate

--- src/components/WQM.tsx
+++ src/components/WQM.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import WQMApiService from '../services/WQMApiService';
+import WorkQueueManagementService from '../services/WorkQueueManagementService';
 
 type Event = {
   id: string;
@@ -7,24 +7,20 @@ type Event = {
   status: 'unlocked' | 'locked';
 };
 
-
-
 const WorkQueueManagement: React.FC = () => {
   const [events, setEvents] = useState<Event[]>([]);
+  const [eventName, setEventName] = useState<string>('');
   const [selectedEventId, setSelectedEventId] = useState<string | null>(null);
 
   const fetchEvents = async () => {
     try {
-      // Mock delay to simulate network request
-      await new Promise(resolve => setTimeout(resolve, 1000));
-  
-      const fetchedEvents: Event[] = [
-        { id: '1', name: 'Event 1', status: 'unlocked' },
-        { id: '2', name: 'Event 2', status: 'locked' },
-        { id: '3', name: 'Event 3', status: 'unlocked' },
-      ];
-  
-      setEvents(fetchedEvents);
+      const fetchedEvents = await WorkQueueManagementService.fetchEvents();
+      if (Array.isArray(fetchedEvents)) {
+        setEvents(fetchedEvents);
+      } else {
+        console.error("Fetched events is not an array:", fetchedEvents);
+        setEvents([]);
+      }
     } catch (error) {
       console.error("Error fetching events:", error);
     }
@@ -33,25 +29,64 @@ const WorkQueueManagement: React.FC = () => {
   const handleLockEvent = async () => {
     if (selectedEventId) {
       try {
-        await WQMApiService.lockEvent(selectedEventId);
-        // Handle success, maybe refetch events or update UI in some way
+        await WorkQueueManagementService.lockEvent(selectedEventId);
+        setTimeout(fetchEvents, 1000);
       } catch (error) {
         console.error("Error locking event:", error);
       }
     }
   };
 
+  const handlePublishEvent = async () => {
+    try {
+      const newEvent: Omit<Event, 'id'> = {
+        name: eventName,
+        status: 'unlocked', // Default status for new events
+      };
+      await WorkQueueManagementService.publish(newEvent);
+      setEventName(''); // Clear the input after publishing
+      fetchEvents(); // Refresh the list of events
+    } catch (error) {
+      console.error("Error publishing event:", error);
+    }
+  };
+
+
   useEffect(() => {
     fetchEvents();
+
+    // Set an interval to fetch events every second
+    const intervalId = setInterval(() => {
+      fetchEvents();
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
+
 
   return (
     <div className="bg-gray-200 p-8 rounded-md shadow-md">
+      <div className="mb-4">
+        <label htmlFor="eventName" className="block text-gray-700">Event Name:</label>
+        <input 
+          type="text" 
+          id="eventName" 
+          value={eventName} 
+          onChange={(e) => setEventName(e.target.value)} 
+          className="mt-1 border rounded-md px-2 py-1 w-full"
+        />
+        <button 
+          onClick={handlePublishEvent} 
+          className="mt-2 bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700"
+        >
+          Publish New Event
+        </button>
+      </div>
       <table className="min-w-full bg-white rounded-md shadow-md overflow-hidden">
         <thead className="bg-gray-900 text-white">
           <tr>
             <th className="py-2 px-4 text-center">Select</th>
-            <th className="py-2 px-4 text-center">Name</th>
+            <th className="py-2 px-4 text-center">Event ID</th>
             <th className="py-2 px-4 text-center">Status</th>
           </tr>
         </thead>
@@ -68,19 +103,17 @@ const WorkQueueManagement: React.FC = () => {
                   className="focus:ring-gray-500"
                 />
               </td>
-              <td className="py-2 px-4 text-center">{event.name}</td>
-              <td className="py-2 px-4 text-center">{event.status ? 'Locked' : 'Unlocked'}</td>
+              <td className="py-2 px-4 text-center">{event.id}</td>
+              <td className="py-2 px-4 text-center">{event.status === 'locked' ? 'locked' : 'unlocked'}</td>
             </tr>
           ))}
         </tbody>
       </table>
       <div className="mt-4 flex space-x-4">
-        <button onClick={fetchEvents} className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-700">Refresh Events</button>
         <button onClick={handleLockEvent} disabled={!selectedEventId} className="bg-gray-700 text-white py-2 px-4 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800">Toggle Lock</button>
       </div>
     </div>
   );
-  
 }
 
 export default WorkQueueManagement;
---
+++ src/services/WorkQueueManagementService.ts
@@ -0,0 +1,59 @@
+class WorkQueueManagementService {
+  private static BASE_URL: string = import.meta.env.VITE_WQM_URL || '/api';  // Default to '/api' if not provided
+
+  static async fetchEvents(minutes: number = 1, limit: number = 10): Promise<any> {
+    const response = await this.request(`${this.BASE_URL}/events?minutes=${minutes}&limit=${limit}`);
+    return response.events || []; // return the events list or an empty list if not present
+  }
+
+  static async lockEvent(eventId: string): Promise<any> {
+    return this.request(`${this.BASE_URL}/lock`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 'event-id': eventId })
+    });
+  }
+
+  static async unlockEvent(eventId: string): Promise<any> {
+    return this.request(`${this.BASE_URL}/unlock`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 'event-id': eventId })
+    });
+  }
+
+  static async publish(payload: any): Promise<any> {
+    return this.request(`${this.BASE_URL}/publish`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    });
+  }
+
+  static async getEvent(eventId: string): Promise<any> {
+    return this.request(`${this.BASE_URL}/get?event-id=${eventId}`);
+  }
+
+
+  private static async request(url: string, options: RequestInit = {}): Promise<any> {
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        const errorData = await response.text();
+        throw new Error(`Server responded with ${response.status}: ${errorData}`);
+      }
+      const jsonData = await response.json();
+      if (jsonData.hasOwnProperty('events')) {
+        return jsonData;
+      } else {
+        // If the expected 'events' key is not present, throw an error (or handle in a way that suits your needs)
+        throw new Error('Expected "events" key not present in response.');
+      }
+    } catch (error) {
+      console.error("Error making request:", error);
+      throw error;
+    }
+  }
+}
+
+export default WorkQueueManagementService;
