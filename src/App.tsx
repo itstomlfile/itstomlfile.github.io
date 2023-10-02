05f89c02fb933c5636075f1c9ac7a397f38115ff
Tom C <tom.charlesworth1997@gmail.com>
2023-10-02 19:41:59 +0100

    Restyle site and add WQM, header and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,22 @@
-import { useForm, ValidationError } from '@formspree/react';
-import { SubmissionError } from '@formspree/core';
-import { useState, ChangeEvent } from 'react';
-import WorkQueueButton from './components/WQM';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ContactForm from './components/ContactForm';
+import WorkQueueManagement from './components/WQM';
+import Header from './components/Header';
 
-function ContactForm() {
-  const FORMSPREE_ID: string | undefined = import.meta.env.VITE_FORMSPREE_ID || "mbjvpdkr";
-  const [name, setName] = useState<string>('');
-  const [state, handleSubmit] = useForm(FORMSPREE_ID as string);
-
-  if (state.succeeded) {
-    return (
-      <div className="text-center mt-10 bg-gray-200 p-8 rounded-md shadow-md">
-        <p className="text-gray-700 font-semibold">Thank you for your message, {name}. I will be in touch ASAP.</p>
-      </div>
-    );
-  }
-
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  }
-
+const App: React.FC = () => {
   return (
-    <form onSubmit={handleSubmit} className="bg-gray-200 p-8 rounded-md shadow-md w-full max-w-lg mx-auto">
-     <label htmlFor="fullname" className="block text-gray-600 font-medium mb-2">
-        Full Name
-      </label>
-      <input
-        id="fullname"
-        type="text"
-        name="fullname"
-        value={name}
-        onChange={handleNameChange}
-        className="w-full p-2 border border-gray-400 rounded-md mb-3 focus:outline-none focus:ring-2 focus:ring-gray-500"
-      />
-      <ValidationError 
-        prefix="Full Name" 
-        field="fullname"
-        errors={state.errors}
-        className="text-red-600"
-      />
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
 
-      <label htmlFor="email" className="block text-gray-600 font-medium mb-2">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-        className="w-full p-2 border border-gray-400 rounded-md mb-3 focus:outline-none focus:ring-2 focus:ring-gray-500"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-        className="text-red-600"
-      />
-
-      <label htmlFor="message" className="block text-gray-600 font-medium mb-2">
-        Message
-      </label>
-      <textarea
-        id="message"
-        name="message"
-        className="w-full p-2 border border-gray-400 rounded-md mb-3 focus:outline-none focus:ring-2 focus:ring-gray-500"
-        rows={4}
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors as SubmissionError<any> | null}
-        className="text-red-600"
-      />
-
-      <button type="submit" disabled={state.submitting} className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-700">
-        Submit
-      </button>
-    </form>
-  );
-
-}
-
-function App() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center space-y-4 bg-gray-100">
-      <ContactForm />
-      <WorkQueueButton />
+      {/* Rest of your application */}
+      <main className="container mx-auto p-5">
+        <Routes>
+          <Route path="/about" element={<h1>Coming Soon!</h1>} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/" element={<WorkQueueManagement />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- src/components/WQM.tsx
+++ src/components/WQM.tsx
@@ -1,13 +1,86 @@
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import WQMApiService from '../services/WQMApiService';
 
-function WorkQueueButton() {
+type Event = {
+  id: string;
+  name: string;
+  status: 'unlocked' | 'locked';
+};
+
+
+
+const WorkQueueManagement: React.FC = () => {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [selectedEventId, setSelectedEventId] = useState<string | null>(null);
+
+  const fetchEvents = async () => {
+    try {
+      // Mock delay to simulate network request
+      await new Promise(resolve => setTimeout(resolve, 1000));
+  
+      const fetchedEvents: Event[] = [
+        { id: '1', name: 'Event 1', status: 'unlocked' },
+        { id: '2', name: 'Event 2', status: 'locked' },
+        { id: '3', name: 'Event 3', status: 'unlocked' },
+      ];
+  
+      setEvents(fetchedEvents);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  const handleLockEvent = async () => {
+    if (selectedEventId) {
+      try {
+        await WQMApiService.lockEvent(selectedEventId);
+        // Handle success, maybe refetch events or update UI in some way
+      } catch (error) {
+        console.error("Error locking event:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+  
   return (
-    <Link to="/work-queue-management">
-      <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700">
-        Work Queue Management
-      </button>
-    </Link>
+    <div className="bg-gray-200 p-8 rounded-md shadow-md">
+      <table className="min-w-full bg-white rounded-md shadow-md overflow-hidden">
+        <thead className="bg-gray-900 text-white">
+          <tr>
+            <th className="py-2 px-4 text-center">Select</th>
+            <th className="py-2 px-4 text-center">Name</th>
+            <th className="py-2 px-4 text-center">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {events.map(event => (
+            <tr key={event.id} className="border-t border-gray-300">
+              <td className="py-2 px-4 text-center">
+                <input
+                  type="radio"
+                  name="selectedEvent"
+                  value={event.id}
+                  checked={selectedEventId === event.id}
+                  onChange={() => setSelectedEventId(event.id)}
+                  className="focus:ring-gray-500"
+                />
+              </td>
+              <td className="py-2 px-4 text-center">{event.name}</td>
+              <td className="py-2 px-4 text-center">{event.locked ? 'Locked' : 'Unlocked'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-4 flex space-x-4">
+        <button onClick={fetchEvents} className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-700">Refresh Events</button>
+        <button onClick={handleLockEvent} disabled={!selectedEventId} className="bg-gray-700 text-white py-2 px-4 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-800">Toggle Lock</button>
+      </div>
+    </div>
   );
+  
 }
 
-export default WorkQueueButton;
+export default WorkQueueManagement;
